6e2bc32cfbeb264aae3d44b220a11ae793b92076
Alexander Mironov <alexan9610@gmail.com>
2022-03-15 19:37:22 +0300

    Realization task

--- main.php
+++ main.php
@@ -1,1 +1,44 @@
 <?php
+
+require_once __DIR__ . '/core/autoload/autoload.php';
+
+use Core\OXEM\Organic\Chicken;
+use Core\OXEM\Organic\Cow;
+use Core\OXEM\Storage\Farm;
+
+$farm = Farm::getInstance();
+
+for ($i = 0; $i < 20; $i++) {
+    $farm->registerAnimal(new Chicken());
+}
+
+for ($i = 0; $i < 10; $i++) {
+    $farm->registerAnimal(new Cow());
+}
+
+echo $farm->getAnimalsCount();
+
+for ($i = 0; $i < 7; $i++) {
+    $farm->gatherAnimalProduct();
+}
+
+echo $farm->getProducts();
+$farm->archiveProducts();
+
+echo "\n**Съездили на рынок**\n\n";
+
+for ($i = 0; $i < 5; $i++) {
+    $farm->registerAnimal(new Chicken());
+}
+
+$farm->registerAnimal(new Cow());
+
+echo $farm->getAnimalsCount();
+
+for ($i = 0; $i < 7; $i++) {
+    $farm->gatherAnimalProduct();
+}
+
+echo $farm->getProducts();
+$farm->archiveProducts();
+echo $farm->getProducts(true);
